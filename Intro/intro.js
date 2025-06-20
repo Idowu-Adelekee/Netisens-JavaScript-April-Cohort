bad445aeea1b6fc7461cd28da212ce81231e1443
Idowu Adeleke <work2idowu@gmail.com>
2025-06-20 18:41:55 +0100

    The first lecture (Introductory class)
    This first lecture was about JavaScript introduction, we then learn variables and values.

---
+++ Intro/intro.js
@@ -0,0 +1,76 @@
+"use strict";
+// Code Commenting (// == single line)
+// Code Commenting (/* */ == mutil lines)
+
+// Variables
+// Defining a variable
+const netisens = "Netisens";
+let mutilple = 2 + 2;
+var halfOf10 = 10 / 2;
+
+// netisens = "ICT";
+console.log(mutilple);
+mutilple = 8;
+console.log(mutilple);
+
+console.log(netisens);
+console.log(halfOf10);
+
+const bigfish = "Big fish";
+const BIGFISH = "Big fish";
+// const big_fish_is_good_for_the_body = "Big fish";
+// Using camelCase
+// const bigFish = "Big fish";
+// const bigFishIsGoodForTheBody = "big";
+
+// //
+// const job1 = "Designer";
+// const job2 = "Developer";
+
+// //
+// const fistJob = "Designer";
+// const secondJob = "Developer";
+
+// const MTT = "Tech";
+
+// // const class = 'Web dev';
+// // const name = "Web dev";
+
+const stringType = "good";
+const numberType = "33";
+const booleanType = true;
+
+let netisensICT;
+
+const nothing = null;
+
+// Two main Data Types
+// 1. Object (functions, objects, arrays);
+// 2 . Primitive
+
+const iAmObject = {
+  firstNam: "Idowu",
+  School: "Uniabuja",
+};
+
+// Primtive
+// Data types in JavaScript
+// 1. Strings
+// 2. Number
+// 3. Boolean
+///////////////////
+// 4. Undefined
+// 5. Null
+// 6. Symbols
+// 7. BigInt
+
+const larger = 9 == 2;
+console.log(larger);
+const myName = "Idowu";
+const noCreditToday = 0;
+
+console.log(typeof stringType);
+console.log(typeof numberType);
+console.log(typeof booleanType);
+console.log(typeof netisensICT);
+console.log(typeof nothing);
---
+++ Intro/note.md
@@ -0,0 +1,19 @@
+/\*
+// Naming a Variable
+i. Always use a camelCase
+ii. Don't start variable with uppercase
+iii. Make sure you variable name is descriptive
+Hard rule
+iv. We can't begin a variable name with a number
+v. Variable can only contain number, letters, underscore and dollar sign;
+vi. Error make occur if you used JavaScript keywords like "new" "class" "function", and name;
+
+// Data types in JavaScript
+// 1. Strings
+// 2. Number
+// 3. Boolean
+///////////////////
+// 4. Undefined
+// 5. Null
+// 6. Symbols
+// 7. BigInt
