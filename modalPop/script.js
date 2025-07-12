2f4e831b1bbdc3d2ace1980f9e6b4e94691f7ae2
Idowu Adeleke <work2idowu@gmail.com>
2025-07-12 08:10:28 +0100

    dom project added

--- modalPop/script.js
+++ modalPop/script.js
@@ -2,15 +2,84 @@
 
 console.log("check in");
 
+// SELECTING ELEMENTS
 const btnShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+console.log(btnCloseModal);
 
-console.log(btnShowModal);
+// ADDING THE FUNCTIONALITY
 
+// Open the pop up
 for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i];
-  console.log(btnShowModal[i]);
+  btnShowModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 }
 
+//Refactoring
+const closeModalFunc = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Closing the pop up
+btnCloseModal.addEventListener("click", closeModalFunc);
+
+overlay.addEventListener("click", function () {
+  closeModalFunc();
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModalFunc();
+  }
+});
+
+/*
+// SELECTING ELEMENTS
+const btnShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+console.log(btnCloseModal);
+
+// ADDING THE FUNCTIONALITY
+
+// Open the pop up
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+//Refactoring 
+
+
+// Closing the pop up
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+overlay.addEventListener("click", function (m) {
+  console.log(m);
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+document.addEventListener("keydown", function (event) {
+  console.log(event);
+  if (event.key === "Escape") {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
+
+*/
 // btnShowModal.addEventListener("click", function () {
 //   console.log("Hello");
 // });
@@ -40,3 +109,9 @@ for (let i = 0; i < btnShowModal.length; i++) {
 //   console.log(odudu);
 //   console.log(arr);
 // });
+
+const helloMachine = function (greeting) {
+  console.log("Hello", greeting);
+};
+
+helloMachine("ladies");
