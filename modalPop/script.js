4b2d871295f05c68aa11a0ae8a0a649d4895e67c
Idowu Adeleke <work2idowu@gmail.com>
2025-07-12 08:07:51 +0100

    Modal Pop Up project has been added

---
+++ modalPop/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+console.log("check in");
+
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+console.log(btnShowModal);
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i];
+  console.log(btnShowModal[i]);
+}
+
+// btnShowModal.addEventListener("click", function () {
+//   console.log("Hello");
+// });
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7];
+// console.log(numbers);
+
+// The for loop
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// The for of loop
+
+// console.log("____The for of loop");
+// for (const value of numbers) {
+//   console.log(value);
+// }
+
+// The functional Programming
+/// The for Each Loop;
+
+// The for Each loop
+// console.log("___The for each loop");
+// numbers.forEach(function (value, odudu, arr) {
+//   console.log(value);
+//   console.log(odudu);
+//   console.log(arr);
+// });
