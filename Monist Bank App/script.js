0eb8a5442cd5514aadd1bc61d31b1014bb5f4e69
Idowu Adeleke <work2idowu@gmail.com>
2025-08-12 12:28:52 +0100

    Transfer, Loan and Closing of Account Functioality has been added

--- Monist Bank App/script.js
+++ Monist Bank App/script.js
@@ -16,7 +16,7 @@ const account2 = {
 };
 const account3 = {
   owner: "Philip Sunday Umoren",
-  movements: [40000, 11000, -90000, 50000, -30000, 90, 1200],
+  movements: [-40000, -11000, -9000, -50000, -30000, -90, -1200],
   interestRate: 0.3, //%
   pin: 3333,
 };
@@ -56,10 +56,19 @@ const labelWelcome = document.querySelector(".label-welcome");
 
 // Buttons
 const btnLogin = document.querySelector(".btn__login");
+const btnTransfer = document.querySelector(".btn-transfer");
+const btnLoan = document.querySelector(".btn-loan");
+const btnCloseAcc = document.querySelector(".btn-close");
+console.log(btnLoan, btnCloseAcc, btnTransfer);
 
 // Inputs
 const inputLoginUsername = document.querySelector(".login-input-username");
 const inputLoginPin = document.querySelector(".login-input-pin");
+const inputTransferUsername = document.querySelector(".input_transfer-user");
+const inputTransferAmount = document.querySelector(".input_transfer-amount");
+const inputLoanAmount = document.querySelector(".input_loan-amount");
+const inputCloseusername = document.querySelector(".input_close-user");
+const inputClosePin = document.querySelector(".input_close-pin");
 
 // THE APP STARTS HERE
 const displayMovement = function (mov) {
@@ -73,29 +82,29 @@ const displayMovement = function (mov) {
             <div class="movements-type movement-type--${type}">${
       i + 1
     } ${type}</div>
-            <div class="movements-value">${mov}</div>
+            <div class="movements-value">${mov.toLocaleString()}</div>
     </div>
     `;
     containerMovementEl.insertAdjacentHTML("afterbegin", html);
   });
 };
 
-const calcDisplayBalance = function (mov) {
-  const balance = mov.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `₦${balance.toLocaleString()}`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `₦${acc.balance.toLocaleString()}`;
 };
 
 const calcDisplaySummary = function (mov) {
   // 1. Total Amount Deposited
   const totalDeposit = mov
     .filter((mov) => mov > 0)
-    .reduce((acc, mov) => acc + mov);
+    .reduce((acc, mov) => acc + mov, 0);
   labelTotalDeposit.textContent = `₦${totalDeposit.toLocaleString()}`;
 
   // 2. Total Amount Withdrawn
   const totalWithdrawal = mov
     .filter((mov) => mov < 0)
-    .reduce((acc, mov) => acc + mov);
+    .reduce((acc, mov) => acc + mov, 0);
   labelTotalWithdrawal.textContent = `₦${Math.abs(
     totalWithdrawal
   ).toLocaleString()}`;
@@ -104,7 +113,7 @@ const calcDisplaySummary = function (mov) {
   const totalInterest = mov
     .filter((mov) => mov > 0)
     .map((mov) => (mov * account4.interestRate) / 100)
-    .reduce((acc, int) => acc + int);
+    .reduce((acc, int) => acc + int, 0);
   labelTotalInterest.textContent = `₦${totalInterest.toLocaleString()}`;
 
   console.log(totalInterest);
@@ -115,7 +124,7 @@ const updateUI = function (acc) {
   displayMovement(currentAccount.movements);
 
   // Display balance
-  calcDisplayBalance(currentAccount.movements);
+  calcDisplayBalance(currentAccount);
 
   // Display the summary
   calcDisplaySummary(currentAccount.movements);
@@ -125,6 +134,15 @@ const updateUI = function (acc) {
 const clearInputs = function () {
   inputLoginPin.value = inputLoginUsername.value = "";
   inputLoginPin.blur();
+
+  inputTransferUsername.value = inputTransferAmount.value = "";
+  inputTransferAmount.blur();
+
+  inputLoanAmount.value = "";
+  inputLoanAmount.blur();
+
+  inputClosePin.value = inputCloseusername.pin = "";
+  inputClosePin.blur();
 };
 
 // IMPLEMENTING LOGIN FUNCTIONALITY
@@ -174,5 +192,69 @@ btnLogin.addEventListener("click", function (e) {
   clearInputs();
 });
 
-// console.log(inputLoginUsername.value);
-// console.log(inputLoginUsername);
+// IMPLEMENTING THE TRANSFER FUNCTIONALITY
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const recipientAccount = accounts.find(
+    (acc) => inputTransferUsername.value === acc.username
+  );
+  const amount = Number(inputTransferAmount.value);
+
+  if (
+    recipientAccount &&
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    recipientAccount !== currentAccount
+  ) {
+    currentAccount.movements.push(-amount);
+    recipientAccount.movements.push(amount);
+
+    // Update the UI
+    updateUI(currentAccount);
+
+    console.log("Transfered");
+  }
+
+  clearInputs();
+});
+
+// IMPLEMENTING REQUESTING LOAN
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov > 0 && amount < mov) &&
+    currentAccount.balance > 0
+  ) {
+    currentAccount.movements.push(amount);
+
+    // Update the UI
+    updateUI(currentAccount);
+  }
+
+  clearInputs();
+});
+
+// IMPLEMENTING THE CLOSING OF ACCOUNT FUNCTIONALITY
+btnCloseAcc.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseusername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      (acc) => currentAccount.username === acc.username
+    );
+
+    accounts.splice(index, 1);
+
+    main.classList.add("no-visible");
+    summary.classList.add("no-visible");
+  }
+
+  clearInputs();
+});
